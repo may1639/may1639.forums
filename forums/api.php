0b9706561066e76980394d1157d4c21fc3058587
RobertKloster <rkloster@iastate.edu>
2015-12-06 16:38:41 -0600

    \answers query begun; veeeery early build...

--- forums/api.php
+++ forums/api.php
@@ -29,45 +29,7 @@ function users()//what should happen if the path starts with 'users'.
 	}
 
 	$query = User::get_query($ids);
-
-	$results = $db->query($query);
-
-	$query = paginate_query($query, mysql_num_rows($results));
-
-	/** /
-	$pagesize = 30;
-	
-	if(isset($_GET['pagesize']))
-	{
-		if(false === filter_input(INPUT_GET, 'pagesize', FILTER_VALIDATE_INT))
-			return_error(400, 'pagesize', 'bad_parameter');
-		
-		$pagesize = max(0, min(100, $_GET["pagesize"]));
-	}
-
-	if(($pagesize < 1) || (mysql_num_rows($results) < 1))
-		return array();
-
-	$pagesize = min($pagesize, mysql_num_rows($results));
-	
-	$hard_limit = mysql_num_rows($results) / $pagesize;
-	
-	if(0 < mysql_num_rows($results) % $pagesize)
-		$hard_limit++;
-	
-	$page = 1;
-	
-	if(isset($_GET["page"]))
-	{
-		if(false === filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT))
-			return_error(400, 'page', 'bad_parameter');
-		
-		$page = max(1, min($hard_limit, $_GET["page"]));
-	}
-	
-	$query .= " LIMIT ".$pagesize." OFFSET ".($page - 1) * $pagesize;
-
-	/**/
+	$query = paginate_query($query, mysql_num_rows($db->query($query)));
 
 	$results = $db->query($query);
 	
@@ -83,11 +45,45 @@ function users()//what should happen if the path starts with 'users'.
 }
 
 
+function answers()
+{
+	global $path, $db;
+
+	if (count($path) > 1)
+	{
+		switch($path[1])
+		{
+			// case 'moderators':
+				
+			// 	break;
+			
+			default:
+				$ids = explode(";", $path[1]);
+				process_ids($ids);
+				break;
+		}
+	}
+
+	$results = $db->query("SELECT p.* FROM mybb_posts p, mybb_users u WHERE u.username=\"adcoats\" && p.uid = u.uid");
+	
+	$retval = array();
+
+	while($row = mysql_fetch_array($results, MYSQL_ASSOC))
+	{		
+		array_push($retval, $row);
+	}
+
+	return $retval;
+}
 
 switch($path[0])//selects proper function to call.
 {
 	case 'users':
 		$return_value = array("items" => users());
+		break;
+	
+	case 'answers':
+		$return_value = array("items" => answers());
 		break;
 	
 	default:
@@ -98,6 +94,8 @@ if(!isset($return_value))
 	return_error(400, 'method', 'method_not_recognized');
 
 /** /
+
+User::func($path);
 
 echo "\nFIN";
 
